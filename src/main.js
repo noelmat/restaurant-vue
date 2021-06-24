04a33f860c0a17fd3bc7a64f3554a9ad49d8a71c
Noel Mathew <noel.mathew007@gmail.com>
2021-06-24 23:21:30 +0530

    registerd router and store to Vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './init';
+import router from '@/router';
+import store from '@/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
