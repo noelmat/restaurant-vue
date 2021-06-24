555b4210d9390f6c9a3c4c8ca773b439fe22733e
Noel Mathew <noel.mathew007@gmail.com>
2021-06-24 21:48:31 +0530

    init

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Vuex from 'vuex';
+
+Vue.use(Router);
+Vue.use(Vuex);
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "normalize.css/normalize.css";
