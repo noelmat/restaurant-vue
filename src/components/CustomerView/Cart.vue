7a744fae642418a28d48cd79d3eaa35e489870e6
Noel Mathew <noel.mathew007@gmail.com>
2021-06-25 17:18:52 +0530

    cart:

---
+++ src/components/CustomerView/Cart.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="head">
+            <h1 class="heading">
+                Cart
+            </h1>
+        </div>
+        <div class="cart-details">
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+        </div>
+        <div class="btn-panel">
+            <a class="btn btn-cta">
+                Place Order
+            </a>
+        </div>
+    </div>
+</template>
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: 'Cart',
+    components: {
+        CartItem
+    }
+}
+</script>
+<style scoped>
+.head{
+    background-color: #555;
+    color: #fff;
+    padding: 1em 2em;
+}
+.heading{
+    margin: 0;
+}
+.cart-details{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 2em;
+}
+.btn{
+    padding: 1em;
+}
+.btn-cta{
+    border-radius: 10px;
+}
+</style>
---
+++ src/components/CustomerView/CartItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cart-item">
+        <div class="name">Item Name</div>
+        <span class="times">x</span>
+        <div class="qty">2</div>
+        <div class="price">20</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CartItem'
+}
+</script>
+<style scoped>
+.cart-item{
+    display: flex;
+    width: 90%;
+    border-radius: 10px;
+    box-shadow: 0 1px 4px 1px rgba(0,0,0,0.2);
+    padding: 1em;
+    justify-content: flex-end;
+    flex-wrap: wrap;
+    margin: .5em 0;
+}
+.times{
+    font-size: .8em;
+}
+.name{
+    width: 100%;
+}
+.qty{
+    margin:0 2em;
+}
+</style>
