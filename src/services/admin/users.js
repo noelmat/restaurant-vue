eee5e959fc7e21496f8fe5e0404c784fb34f5030
Noel Mathew <noel.mathew007@gmail.com>
2021-06-27 14:16:14 +0530

    improv: configured axios instance for admin

--- src/services/admin/users.js
+++ src/services/admin/users.js
@@ -1,35 +1,20 @@
-import axios from 'axios';
+import service from './configureAxios';
 
 export const postUser = (user)=>{
-    return axios.post(
+    return service({requiresAuth: true}).post(
         'http://localhost:3000/auth/usermanagement/register',
         user,
-        {
-            headers: {
-                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW4gVXNlciIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYyNDAxNzI2MH0.lvdOfBxhCgTikdFy32YNx8VEV1wxwKCIknOhjVaaDPA'
-            }    
-        }
     )
 }
 
 export const getUsers = () => {
-    return axios.get(
+    return service({requiresAuth: true}).get(
         'http://localhost:3000/auth/usermanagement/',
-        {
-            headers: {
-                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW4gVXNlciIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYyNDAxNzI2MH0.lvdOfBxhCgTikdFy32YNx8VEV1wxwKCIknOhjVaaDPA'
-            }
-        }
-    ).then(res => res.data)
+    )
 }
 export const updateUserDetails = (id, user)=>{
-    return axios.patch(
+    return service({requiresAuth: true}).patch(
         `http://localhost:3000/auth/usermanagement/users/${id}`,
         user,
-        {
-            headers: {
-                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW4gVXNlciIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYyNDAxNzI2MH0.lvdOfBxhCgTikdFy32YNx8VEV1wxwKCIknOhjVaaDPA'
-            }    
-        }
     )
 }
